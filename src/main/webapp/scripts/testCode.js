89307239d5c5b93103c7ad46ee8e31f99d6b2260
eggsy <eggsy@eggsylife.co.uk>
2011-04-18 12:48:06 +0100

    Add licenses and further info to README

--- src/main/webapp/scripts/testCode.js
+++ src/main/webapp/scripts/testCode.js
@@ -1,6 +1,6 @@
 
 /** Test Javascript that will be obfuscated ***/
-/** Add this comment will be removed of course ****/
+/** And this comment will be removed of course ****/
 function hello(name) {
   alert('Hello, ' + name);
 }
